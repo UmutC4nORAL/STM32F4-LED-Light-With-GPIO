09426075344dc24c687c23e780edd0c22c207fc2
Umut Can <uoral036@gmail.com>
2024-07-21 16:05:55 +0300

    comment lines added

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 
 int main(){
 	volatile int i;
-	RCC_AHB1ENR |= 1U << 0;
-	RCC_AHB1ENR |= 1U << 3;
-	GPIOD_MODER |= 1U << 30;
+	RCC_AHB1ENR |= 1U << 0; // Activate AHB1 clock
+	RCC_AHB1ENR |= 1U << 3; // Activate GPIO D port
+	GPIOD_MODER |= 1U << 30; // Set pin 15 of GPIO D port to output mode
 	while(1){
-		if(GPIOA_IDR & (1U <<0)){
-			GPIOD_ODR |= 1U << 15;
+		if(GPIOA_IDR & (1U <<0)){ // Check pin 0 of GPIO A port
+			GPIOD_ODR |= 1U << 15; // Light on blue LED
 			for(i=0;i<1000000;i++);
-			GPIOD_ODR &= ~(1U << 15);
+			GPIOD_ODR &= ~(1U << 15); // Light off the blue LED
 		}
 	}
 }
